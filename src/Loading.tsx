edf705602e50e8a387e638ce82c204bb59bdeaa3
Nathan Bosscher <nate.bosscher@gmail.com>
2020-08-28 11:53:55 -0400

    format and setup for npm

---
+++ src/Loading.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { CircularProgress } from '@material-ui/core';
+
+interface Props {
+  smaller?: boolean;
+}
+
+export class Loading extends React.Component<Props, {}> {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <CircularProgress size={this.props.smaller ? '20px' : undefined} />
+      </div>
+    );
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./APIBase"
+export * from "./AsyncUtils"
+export * from "./Auth"
+export * from "./EventEmitter"
+export * from "./Fetcher"
+export * from "./Loading"
