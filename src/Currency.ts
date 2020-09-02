f7794d09e0a22f0a0e55af2a61467821ff52d087
Nathan Bosscher <nate.bosscher@gmail.com>
2020-09-02 13:39:44 -0400

    fix issue with currency parsing into NaN

--- src/Currency.ts
+++ src/Currency.ts
@@ -12,11 +12,14 @@ export function formatCents(n: Cents | undefined) {
 
 export function parseCents(str: string): number {
   const parts = str.split('.');
-  const dollars = parseInt(parts[0], 10);
+  let dollars = parseInt(parts[0], 10);
   let cents = 0;
   if (parts.length >= 2) {
     cents = parseInt(parts[1].substring(0, 2).padEnd(2, '0'), 10);
   }
+
+  if(isNaN(dollars)) dollars = 0;
+  if(isNaN(cents)) cents = 0;
 
   return Math.round(dollars * 100 + cents);
 }
