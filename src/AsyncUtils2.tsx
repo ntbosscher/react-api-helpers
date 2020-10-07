0e9c5375d8b9ed7de77a59f168dd0ea5e4cf7a58
Nathan Bosscher <nate.bosscher@gmail.com>
2020-10-07 12:39:22 -0400

    v1.6.1

--- src/AsyncUtils2.tsx
+++ src/AsyncUtils2.tsx
@@ -1,31 +1,39 @@
+import { PropsWithChildren } from 'react';
 import { usePaginatedQuery } from 'react-query';
 import { LoadingEl } from './AsyncUtils';
+import {
+  PaginatedQueryConfig,
+  PaginatedQueryResult,
+  QueryKey,
+  TypedQueryFunction,
+  TypedQueryFunctionArgs,
+} from 'react-query/types/core/types';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
+import React from 'react';
 
-type Callback<T> = (page: number) => Promise<T[]>;
+const queryCache = new QueryCache();
 
-export function useAsyncPaginated<T>(
-  key: string,
-  page: number,
-  callback: Callback<T>,
-): {
-  LoadingOrErrorElement: JSX.Element | null;
-  current: T[];
-  isPreviousData: boolean;
-  clear(): void;
-  canFetchMore: boolean;
+export function AsyncCacheProvider(props: PropsWithChildren<{}>) {
+  return <ReactQueryCacheProvider queryCache={queryCache}>{props.children}</ReactQueryCacheProvider>;
+}
+
+export function useAsyncPaginated<TResult, TError extends Error, TArgs extends TypedQueryFunctionArgs>(
+  queryKey: QueryKey,
+  queryFn: TypedQueryFunction<TResult, TArgs>,
+  queryConfig?: PaginatedQueryConfig<TResult, TError>,
+): PaginatedQueryResult<TResult, TError> & {
+  autoShow: (dataView: JSX.Element) => JSX.Element;
+  LoadingOrError: JSX.Element | null;
 } {
-  const result = usePaginatedQuery([key, page, callback], (key, page: number = 0, callback: Callback<T>) =>
-    callback(page),
-  );
+  const result = usePaginatedQuery(queryKey, queryFn, queryConfig);
+  const loading =
+    result.isError || result.isLoading
+      ? LoadingEl(result.isLoading, result.error?.message as string, () => result.refetch())
+      : null;
 
   return {
-    LoadingOrErrorElement:
-      result.isError || result.isLoading
-        ? LoadingEl(result.isLoading, (result.error as Error | undefined)?.message as string, () => result.refetch())
-        : null,
-    current: result.resolvedData || [],
-    isPreviousData: result.isPreviousData,
-    clear: result.clear,
-    canFetchMore: result.canFetchMore || false,
+    ...result,
+    autoShow: loading === null ? (value: JSX.Element) => value : (dataView: JSX.Element) => loading,
+    LoadingOrError: loading,
   };
 }
