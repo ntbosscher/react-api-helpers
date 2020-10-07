1fe973459996fdb7853d7f32351ee0aaa3bdc171
Nathan Bosscher <nate.bosscher@gmail.com>
2020-10-07 10:15:03 -0400

    add last ping result to auth provider

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -4,11 +4,13 @@ import { notAuthorizedResponse } from './APIBase';
 
 interface Auth {
   authenticated: boolean;
+  lastPingResult: any | null;
   setAuthenticated(tf: boolean): void;
 }
 
 const AuthContext = createContext<Auth>({
   authenticated: false,
+  lastPingResult: null,
   setAuthenticated(tf: boolean) {
     console.error('auth context not configured');
   },
@@ -16,18 +18,19 @@ const AuthContext = createContext<Auth>({
 
 export function AuthProvider(
   props: PropsWithChildren<{
-    onPing?: () => any;
+    onPing?: () => Promise<any>;
     pingInterval?: number; // default: every 30 sec
   }>,
 ) {
   const [authenticated, setAuthenticated] = useState(true);
-  const ctx = useMemo(() => ({ authenticated, setAuthenticated }), [authenticated, setAuthenticated]);
+  const [lastPingResult, setLastPingResult] = useState(null);
 
   let { onPing, pingInterval } = props;
 
   useEffect(() => {
     if (!onPing) return;
-    const checker = setInterval(onPing, pingInterval || 30 * 1000);
+    const ping = onPing;
+    const checker = setInterval(async () => setLastPingResult(await ping()), pingInterval || 30 * 1000);
     return () => clearInterval(checker);
   }, [onPing, pingInterval]);
 
@@ -38,6 +41,8 @@ export function AuthProvider(
 
     return () => sub.cancel();
   }, []);
+
+  const ctx = useMemo(() => ({ authenticated, setAuthenticated, lastPingResult }), [authenticated, setAuthenticated, lastPingResult]);
 
   return <AuthContext.Provider value={ctx}>{props.children}</AuthContext.Provider>;
 }
