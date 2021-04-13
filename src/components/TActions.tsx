0d9c6893aa96d4a392cb51da653d8e53739c5be7
Nathan Bosscher <nate.bosscher@gmail.com>
2021-04-13 14:35:47 -0400

    v1.9.5

--- src/components/TActions.tsx
+++ src/components/TActions.tsx
@@ -2,19 +2,17 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-export function TActions(props: {
-  loadingOrError: JSX.Element | null;
-  onCancel?: () => void;
-  confirmText?: string
-}) {
+export function TActions(props: { loadingOrError: JSX.Element | null; onCancel?: () => void; confirmText?: string }) {
   return (
     <Grid container justify="flex-end" spacing={2}>
       {props.loadingOrError && <Grid item>{props.loadingOrError}</Grid>}
-      {props.onCancel && <Grid item>
-        <Button variant="contained" onClick={props.onCancel}>
-          Cancel
-        </Button>
-      </Grid>}
+      {props.onCancel && (
+        <Grid item>
+          <Button variant="contained" onClick={props.onCancel}>
+            Cancel
+          </Button>
+        </Grid>
+      )}
       <Grid item>
         <Button variant="contained" color="primary" type="submit">
           {props.confirmText || 'Save'}
