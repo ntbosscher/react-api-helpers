b7cdef602edb3cca2027006f8084f0ab9762656f
Nathan Bosscher <nate.bosscher@gmail.com>
2021-06-11 13:21:54 -0400

    v1.9.26

--- src/components/USelect.tsx
+++ src/components/USelect.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { FormControl, InputLabel, Select } from '@material-ui/core';
+import { FormControl, InputLabel, Select, SelectProps } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -8,15 +8,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export function USelect<T>(props: {
-  style?: React.CSSProperties;
-  label: string;
-  onChange?: (value: any) => void;
-  children: JSX.Element[];
-  variant?: 'filled' | 'outlined';
-  value?: any;
-  required?: boolean;
-}) {
+export function USelect<T>(
+  props: {
+    style?: React.CSSProperties;
+    label: string;
+    onChange?: (value: any) => void;
+    children: JSX.Element[];
+  } & Omit<SelectProps, 'onChange'>,
+) {
   const styles = useStyles();
 
   return (
@@ -29,11 +28,7 @@ export function USelect<T>(props: {
       >
         {props.label}
       </InputLabel>
-      <Select
-        required={props.required}
-        value={props.value}
-        onChange={(e) => props.onChange && props.onChange(e.target.value)}
-      >
+      <Select {...props} onChange={(e) => props.onChange && props.onChange(e.target.value)}>
         {props.children}
       </Select>
     </FormControl>
